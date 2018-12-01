89964f2cae70c490e859038185b783c093723b20
Eiliya <eiliya.abedini2009@gmail.com>
2018-12-01 17:42:27 +0330

    Step 1: Simple DataProvider

---
+++ app/src/main/java/ir/iact/koinSample/DataProvider.kt
@@ -0,0 +1,7 @@
+package ir.iact.koinSample
+
+class DataProvider {
+    fun getUserName(): String {
+        return "Eiliya"
+    }
+}
--- app/src/main/java/ir/iact/koinSample/MainActivity.kt
+++ app/src/main/java/ir/iact/koinSample/MainActivity.kt
@@ -2,11 +2,21 @@ package ir.iact.koinSample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val dataProvider: DataProvider = DataProvider()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getUsernameButton.setOnClickListener {
+            val userName = dataProvider.getUserName()
+            Toast.makeText(this, "Username is: $userName", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
